8eac8651a24e3a9356ad88dd42a0311df15d72bb
Jade Montesano <j@montesano.dev>
2020-12-01 02:05:10 -0500

    Delete global.js
    Deleting unnecessary files...

--- global.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    currentScreen: null,            // which app screen is open?  settings, contacts, call
-    activeCall: null,               // is there an active call? (may not be used)
-
-    darkMode: false,                // high contrast dark mode ADA Compliant
-    dyslexic: false,                // dyslexia friendly font ADA Compliant
-    contactsList: null
- };
-
